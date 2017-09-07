6739e4d5faa60814859196302a9ff28aac3b48c2
Michele Cipriano <micco00x@gmail.com>
2017-09-07 15:26:23 +0200

    Pretrained embedding matrix in decoder

--- src/seq2seq/Decoder.py
+++ src/seq2seq/Decoder.py
@@ -1,12 +1,15 @@
 import torch
 
 class Decoder(torch.nn.Module):
-	def __init__(self, hidden_size, output_size, n_layers):
+	def __init__(self, hidden_size, output_size, n_layers, embedding_matrix=None):
 		super(Decoder, self).__init__()
 		self.hidden_size = hidden_size
 		self.n_layers = n_layers
 
 		self.embedding = torch.nn.Embedding(output_size, hidden_size)
+		if embedding_matrix != None:
+			self.embedding.weight.data.copy_(torch.from_numpy(embedding_matrix))
+		
 		self.lstm = torch.nn.LSTM(hidden_size, hidden_size)
 		self.out = torch.nn.Linear(hidden_size, output_size)
 		self.softmax = torch.nn.LogSoftmax()
