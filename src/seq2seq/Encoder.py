9bd4ef171eee23340451e50435d2d450640fe5b4
Michele Cipriano <micco00x@gmail.com>
2017-09-07 12:18:49 +0200

    Encoder modified

---
+++ src/seq2seq/Encoder.py
@@ -0,0 +1,23 @@
+import torch
+
+class Encoder(torch.nn.Module):
+	def __init__(self, input_size, hidden_size, n_layers, embedding_matrix=None):
+		super(Encoder, self).__init__()
+		self.hidden_size = hidden_size
+		self.n_layers = n_layers
+
+		self.embedding = torch.nn.Embedding(input_size, hidden_size)
+		if embedding_matrix != None:
+			self.embedding.weight.data.copy_(torch.from_numpy(embedding_matrix))
+
+		self.lstm = torch.nn.LSTM(hidden_size, hidden_size)
+
+	def forward(self, input, hidden):
+		embedded = self.embedding(input).view(1, 1, -1)
+		output = embedded
+		for i in range(self.n_layers):
+			output, hidden = self.lstm(output, hidden)
+		return output, hidden
+
+	def init_hidden(self):
+		return torch.autograd.Variable(torch.zeros(1, 1, self.hidden_size))
