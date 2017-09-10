204618c7c4993248f03bb60b84db8bade461aad3
Michele Cipriano <micco00x@gmail.com>
2017-09-10 12:13:09 +0200

    Word2Vec.py modified

--- src/Word2Vec.py
+++ src/Word2Vec.py
@@ -2,60 +2,39 @@ from gensim.models.keyedvectors import KeyedVectors
 
 import numpy as np
 
-from utils import split_words_punctuation
+import utils
+from Vocabulary import Vocabulary
 
 # Word2Vec class which supports word2vec model from Google with embedding dim. of 300:
 class Word2Vec:
 	def __init__(self, word2vec_path, vocabulary_path):
 		
+		# Read word2vec binary file:
 		vecmodel = KeyedVectors.load_word2vec_format(word2vec_path, binary=True)
 		self.EMBEDDING_DIM = 300
 		
-		vocabulary = []
-		with open(vocabulary_path) as vocabulary_file:
-			for w in vocabulary_file:
-				vocabulary.append(w.rstrip("\n"))
-	
-		veclist = []
-		wordlist = []
+		# Set up vocabulary:
+		self.vocabulary = Vocabulary(vocabulary_path)
 		
-		for word in vocabulary:
+		# Set up embedding_matrix:
+		self.embedding_matrix = np.zeros((self.vocabulary.VOCABULARY_DIM, self.EMBEDDING_DIM))
+		
+		for word, idx in vocabulary.word2index.items():
 			try:
-				if word not in wordlist:
-					wordvec = vecmodel[word]
-					veclist.append(wordvec)
-					wordlist.append(word)
+				self.embedding_matrix[idx] = vecmodel[word]
 			except KeyError:
-				# TO TEST:
-				wordvec = [0] * self.EMBEDDING_DIM
-				veclist.append(wordvec)
-				wordlist.append(word)
-				#pass
-
-		self.UNK_SYMBOL = 0
-		self.GO_SYMBOL = 1
-		self.EOS_SYMBOL = 2
-		
-		self.VOCABULARY_DIM = len(wordlist) + 3
-		
-		self.word_to_indices = dict((c, i) for i, c in enumerate(wordlist))
-		#self.indices_to_word = dict((i, c) for i, c in enumerate(wordlist))
-
-		self.embedding_matrix = np.zeros((self.VOCABULARY_DIM, self.EMBEDDING_DIM))
-		
-		for i in range(len(wordlist)):
-			self.embedding_matrix[i + 3] = veclist[i]
+				self.embedding_matrix[idx] = [0] * self.EMBEDDING_DIM
 
 	# Word to index (+1 because of OOV word):
 	def w2i(self, word):
-		if word in self.word_to_indices:
-			return self.word_to_indices[word] + 3
+		if word in self.vocabulary.word2index:
+			return self.vocabulary.word2index[word]
 		else:
 			return 0
 
-	# Sentence to word embeddings (indices):
+	# Sentence to indices:
 	def s2i(self, sentence):
 		q = []
-		for w in split_words_punctuation(sentence):
+		for w in utils.split_words_punctuation(sentence):
 			q.append(self.w2i(w))
 		return q
