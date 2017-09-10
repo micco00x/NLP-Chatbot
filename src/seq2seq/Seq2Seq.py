fc56d4f402f1112fcf941c67c525bb8cd867d0f6
Michele Cipriano <micco00x@gmail.com>
2017-09-10 12:37:53 +0200

    Seq2Seq updated

--- src/seq2seq/Seq2Seq.py
+++ src/seq2seq/Seq2Seq.py
@@ -7,29 +7,31 @@ from seq2seq import Encoder, Decoder
 class Seq2Seq:
 	def __init__(self,
 				 #encoder, decoder, encoder_optimizer, decoder_optimizer, criterion,
-				 word2vec):
+				 vocabulary_dim, go_symbol_idx, eos_symbol_idx,
+				 embedding_dim, embedding_matrix_encoder, embedding_matrix_decoder):
 		# hparams:
-		encoder_input_size = word2vec.VOCABULARY_DIM
-		encoder_hidden_size = word2vec.EMBEDDING_DIM
+		encoder_input_size = vocabulary_dim
+		encoder_hidden_size = embedding_dim
 		encoder_n_layers = 1
 		
-		decoder_hidden_size = word2vec.EMBEDDING_DIM
-		decoder_output_size = word2vec.VOCABULARY_DIM
+		decoder_hidden_size = embedding_dim
+		decoder_output_size = vocabulary_dim
 		decoder_n_layers = 1
 		
-		# TODO: move GO_SYMBOL, EOS_SYMBOL, embedding_matrix, etc. as hparams
+		self.GO_SYMBOL_IDX = go_symbol_idx
+		self.EOS_SYMBOL_IDX = eos_symbol_idx
 	
 		# Encoder:
 		self.encoder = Encoder.Encoder(encoder_input_size,
 									   encoder_hidden_size,
 									   encoder_n_layers,
-									   word2vec.embedding_matrix)
+									   embedding_matrix)
 									   
 		# Decoder:
 		self.decoder = Decoder.Decoder(decoder_hidden_size,
 									   decoder_output_size,
 									   decoder_n_layers,
-									   word2vec.embedding_matrix)
+									   embedding_matrix)
 											   
 		# Optimizers:
 		self.encoder_optimizer = torch.optim.RMSprop(self.encoder.parameters())
@@ -37,14 +39,14 @@ class Seq2Seq:
 		
 		# Loss:
 		self.criterion = torch.nn.NLLLoss()
-		
-		self.word2vec = word2vec
 
 	def train(self, X, Y, epochs): # TODO: add X_dev=None, Y_dev=None, batch_size
+		print_every = 500
 		for epoch in range(epochs):
 			# TODO: add padding when using batches
 			print("Epoch", epoch+1)
 			cnt = 0
+			tot_loss = 0
 			for x, y in zip(X, Y):
 				cnt += 1
 			
@@ -64,7 +66,7 @@ class Seq2Seq:
 					encoder_output, encoder_hidden = self.encoder(x[ei], encoder_hidden)
 					#encoder_outputs[ei] = encoder_output[0][0]
 
-				decoder_input = torch.autograd.Variable(torch.LongTensor([[self.word2vec.GO_SYMBOL]]))
+				decoder_input = torch.autograd.Variable(torch.LongTensor([[self.GO_SYMBOL_IDX]]))
 				decoder_hidden = encoder_hidden
 
 				# Without teacher forcing:
@@ -76,14 +78,19 @@ class Seq2Seq:
 					decoder_input = torch.autograd.Variable(torch.LongTensor([[ni]]))
 
 					loss += self.criterion(decoder_output, y[di])
-					if ni == self.word2vec.EOS_SYMBOL:
+					if ni == self.EOS_SYMBOL_IDX:
 						break
+
+				tot_loss += loss.data[0]
+				if cnt % print_every == 0:
+					print("Avg. loss at iteration " + str(cnt) + ": " + str(tot_loss/print_every))
+					tot_loss = 0
 
 				loss.backward()
 
 				self.encoder_optimizer.step()
 				self.decoder_optimizer.step()
 
-				print("Loss (" + str(cnt) + "): " + str(loss), end="\r")
+				#print("Loss (" + str(cnt) + "): " + str(loss), end="\r")
 			print("Loss:", loss)
 			
